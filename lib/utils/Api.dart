c7ffd65bb2fa4b9789e3738188ee4499e52adc82
Thuan Nguyen Thai Ninh <thuanntn@nal.vn>
2021-04-24 15:15:29 +0700

    Init firebase config

--- lib/utils/Api.dart
+++ lib/utils/Api.dart
@@ -7,4 +7,29 @@ import 'package:keepapp/utils/AppConstants.dart';
 import 'package:keepapp/utils/Exceptions.dart';
 import 'package:keepapp/utils/Utils.dart';
 
+String parent =
+    "projects/ntnt-1999/databases/(default)/documents/notes/${Utils.userId}/data/";
 
+/// To work on Notes List
+String NOTE_API =
+    "https://firestore.googleapis.com/v1/projects/ntnt-1999/databases/(default)/documents/notes/${Utils.userId}/data/";
+
+/// To work on Notes List
+String USER_DATA_API =
+    "https://firestore.googleapis.com/v1/projects/ntnt-1999/databases/(default)/documents/notes/${Utils.userId}/";
+
+/// Register User
+const registerUserApi =
+    "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${AppConstants.API_KEY}";
+
+/// Login Existing User with Email and Password
+const signInApi =
+    "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${AppConstants.API_KEY}";
+
+/// Login Existing User with Token
+const loginWithApiToken =
+    "https://www.googleapis.com/identitytoolkit/v3/relyingparty/getAccountInfo?key=${AppConstants.API_KEY}";
+
+/// Send reset password email to user
+const passwordResetApi =
+    "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=${AppConstants.API_KEY}";
